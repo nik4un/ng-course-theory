284d04c9d404d5becbb5f92a252369c822b0581d
nik4un <nik4un@yandex.ru>
2018-05-29 12:31:41 +0300

    p1-b2-3 [Селекторы в компонентах]

--- src/app/car/car.component.ts
+++ src/app/car/car.component.ts
@@ -4,14 +4,8 @@ import { Component, OnInit } from '@angular/core';
   selector: 'app-car',
   template: `
     <h2>Single Car</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus, nesciunt.</p>
   `,
-  // styleUrls: ['./car.component.css']
-  styles: [`
-    h2 {
-      color: lightblue;
-    }
-  `]
+  styleUrls: ['./car.component.css']
 })
 export class CarComponent implements OnInit {
 
