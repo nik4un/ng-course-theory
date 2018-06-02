c632f46ac62ba034cee7ba15dda853a97f11ffa0
Nik <nik@iMac.local>
2018-06-02 22:32:59 +0300

    p1-b4-1 [Создание своей директивы]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,13 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { CarsComponent } from './cars/cars.component';
-import { CarComponent } from './car/car.component';
-import { AddCarComponent } from './add-car/add-car.component';
+import { BackgroundDirective } from './directives/background.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CarsComponent,
-    CarComponent,
-    AddCarComponent
-],
+    BackgroundDirective
+  ],
   imports: [
     BrowserModule,
     FormsModule,
