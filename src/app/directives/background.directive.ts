04d6d6cce65d4fd0364e18be33810df6a05c5499
Nik <nik@iMac.local>
2018-06-03 10:33:02 +0300

    p1-b4-3 [Работа с событиями]

--- src/app/directives/background.directive.ts
+++ src/app/directives/background.directive.ts
@@ -2,18 +2,41 @@ import {
   Directive,
   ElementRef,
   OnInit,
-  Renderer2
+  Renderer2,
+  HostListener,
+  HostBinding
 } from '@angular/core';
 
 @Directive({
   selector: '[appBackground]'
 })
 export class BackgroundDirective implements OnInit {
+
+
+  @HostBinding('style.border') border: string; // Способ 2 стилем через директивы без использования рендерера
+
   constructor(private element: ElementRef, private renderer: Renderer2) {}
 
   ngOnInit() {
+
+  }
+
+  // Эта конструкция, когда нужно использовать событие
+  // @HostListener('mouseenter', ['$event']) onmouseenter(evt: Event) {
+  //   console.log(evt);
+  // }
+
+  @HostListener('mouseenter') onMousEenter() {
     const { nativeElement } = this.element;
-    this.renderer.addClass(nativeElement, 'white-text')
-    this.renderer.setStyle(nativeElement, 'background-color', 'lightblue');
+    this.renderer.addClass(nativeElement, 'white-text');
+    this.renderer.setStyle(nativeElement, 'background-color', 'red');
+    this.border = '2px solid green'; // Способ 2
+  }
+
+  @HostListener('mouseleave') onMousLeave() {
+    const { nativeElement } = this.element;
+    this.renderer.removeClass(nativeElement, 'white-text');
+    this.renderer.setStyle(nativeElement, 'background-color', 'transparent');
+    this.border = 'none'; // Способ 2
   }
 }
