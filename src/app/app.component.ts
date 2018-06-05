3f76fdfa3c0f0e7e6da849e993006ebce6110788
nik4un <nik4un@yandex.ru>
2018-06-05 13:50:21 +0300

    p1-b5-2 [Создание своего пайпа]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,47 +4,14 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   template: `
     <div class="col-xs-8 col-xs-offset-2">
-      <h6>{{ name }}</h6>
-      <h6>{{ name | lowercase }}</h6>
-      <h6>{{ name | uppercase }}</h6>
-      <h6>{{ name | slice:6:10 }}</h6>
-      <h6>{{ name | slice:6:10 | uppercase }}</h6>
-      <hr>
-      <h6>{{ pi }}</h6>
-      <h6>{{ pi | number }}</h6>
-      <h6>{{ pi | number:'1.3-4' }}</h6>
-      <h6>{{ pi | number:'2.2-2' }}</h6>
-      <hr>
-      <h6>{{ money | currency:'USD' }}</h6>
-      <h6>{{ money | currency:'EUR':false }}</h6>
-      <hr>
-      <h6>{{ date }}</h6>
-<<<<<<< HEAD
-=======
-      <h6>{{ date | date}}</h6>
-      <hr>
-      <h6>{{ amount }}</h6>
-      <h6>{{ amount | percent}}</h6>
-      <hr>
-      <h6>{{ object }}</h6>
-      <pre>{{ object | json }}</pre><!--JSON.stringify(object)-->
->>>>>>> a0fc70b525b90201567b7b225e2b726f56b6c6f2
+      <h6>{{ num }}</h6>
+      <h6>{{ num | appPow:3:'=' }}</h6>
+      <h6>{{ num | appPow }}</h6>
+      <h6>{{ num | appPow:4 }}</h6>
+      <h6>{{ 6 | appPow:2 }}</h6>
     </div>
   `
 })
 export class AppComponent {
-  name = 'Hello Word!';
-  pi = Math.PI;
-  money = 350;
-  date = new Date();
-  amount = 0.45;
-  object = {
-    foo: 'bar',
-    baz: 'qux',
-    nested:
-      {
-        xyz: 3,
-        numbers: [1, 2, 3]
-      }
-  };
+  num = 2;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { PowPipe } from './pow.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PowPipe
   ],
   imports: [
     BrowserModule,
