e94b60dec18f4d2f94d787b5c25d84372fad139c
nik4un <nik4un@yandex.ru>
2018-06-05 12:28:20 +0300

    ?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,16 +2,39 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div class="col-xs-8 col-xs-offset-2">
+      <h6>{{ name }}</h6>
+      <h6>{{ name | lowercase }}</h6>
+      <h6>{{ name | uppercase }}</h6>
+      <h6>{{ name | slice:6:10 }}</h6>
+      <h6>{{ name | slice:6:10 | uppercase }}</h6>
+      <hr>
+      <h6>{{ pi }}</h6>
+      <h6>{{ pi | number }}</h6>
+      <h6>{{ pi | number:'1.3-4' }}</h6>
+      <h6>{{ pi | number:'2.2-2' }}</h6>
+      <hr>
+      <h6>{{ money | currency:'USD' }}</h6>
+      <h6>{{ money | currency:'EUR':false }}</h6>
+      <hr>
+      <h6>{{ date }}</h6>
+    </div>
+  `
 })
 export class AppComponent {
-  headerText = 'Создание своей директивы.';
-  items = [1, 2, 3, 4, 5];
-
-  current = 1;
-
-  onClick(num: number) {
-    this.current = num;
-  }
+  name = 'Hello Word!';
+  pi = Math.PI;
+  money = 350;
+  date = new Date();
+  amount = 0.45;
+  object = {
+    foo: 'bar',
+    baz: 'qux',
+    nested:
+      {
+        xyz: 3,
+        numbers: [1, 2, 3]
+      }
+  };
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BackgroundDirective } from './directives/background.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BackgroundDirective
+    AppComponent
   ],
   imports: [
     BrowserModule,
