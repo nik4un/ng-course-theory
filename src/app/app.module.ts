4c08615e243d7577ccce713f017cdbfddbd106fa
Nik <nik@iMac.local>
2018-06-15 09:55:13 +0300

    p1-b10-2 [Создание общего модуля]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,26 +6,27 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './app-routing.module';
-import { ColorDirective } from './shared/color.directive';
 import { HeaderComponent } from './header/header.component';
 import { CarsModule } from './cars-page/cars.module';
+import { SharedModule } from './shared/shared.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    ColorDirective,
-    HeaderComponent
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
-    CarsModule // здесь подключается созданный нами модуль
+    CarsModule, // здесь подключается созданный нами модуль
+    SharedModule // а также общий модуль
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
--- src/app/cars-page/cars.module.ts
+++ src/app/cars-page/cars.module.ts
@@ -5,6 +5,7 @@ import { CarPageComponent } from './car-page/car-page.component';
 import { CarsPageComponent } from './cars-page.component';
 import { CarsService } from '../cars.service';
 import { CarsRoutingModule } from './cars-routing.module';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -15,9 +16,11 @@ import { CarsRoutingModule } from './cars-routing.module';
     CommonModule,
     // при создании нового модуля нужно не забыть создать routing module
     // и зарегистрировать его здесь
-    CarsRoutingModule
+    CarsRoutingModule,
+    SharedModule
   ],
   providers: [CarsService],
 })
 export class CarsModule {}
 // при создании нового модуля нужно не забыть создать routing module
+// и общий модуль
