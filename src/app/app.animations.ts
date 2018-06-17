8a91267f42dcddfec2fdaa276422bcb77575321c
Nik <nik@iMac.local>
2018-06-17 21:05:05 +0300

    p1-b11-5 [Специальные символы]

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,10 +1,8 @@
-import { animate, state, style, transition, trigger } from '@angular/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 export const divTrigger = trigger('divTrigger', [
-  state('show', style({
-    opacity: 1 // это свойство можно не указывать, т.к. у всех элементов по-умолчонию оно 1
-  })),
-  transition('void => show', [
+  // 'void => *  -->  ':enter'
+   transition(':enter', [
     style({
       opacity: 0
     }),
@@ -12,7 +10,22 @@ export const divTrigger = trigger('divTrigger', [
       opacity: 1
     }))
   ]),
-  transition('show => void', animate(500, style({
+  // '* => void' --> ':leave'
+  transition(':leave', animate(500, style({
     opacity: 0
   })))
+]);
+// * здесь - это любое состояние;
+// void => * - переход из void в любое состояние;
+// * => void - переход из любого состояния в void
+
+export const changeWithTrigger = trigger('changeWith', [
+  transition('* => *', [
+    animate(1000, style({
+      width: '10px'
+    })),
+    animate(1000, style({
+      width: '*' // * обозначает изначальную ширину или высоту
+    }))
+  ])
 ]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { divTrigger } from './app.animations';
+import { divTrigger, changeWithTrigger } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [divTrigger]
+  animations: [divTrigger, changeWithTrigger]
 })
 export class AppComponent {
   isVisible = false;
