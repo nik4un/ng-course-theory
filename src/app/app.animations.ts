e49184131459d0c7eb6688eeadfd89fc977781f0
Nik <nik@iMac.local>
2018-06-17 21:57:26 +0300

    p1-b11-6 [Детальное управление анимацией]

--- src/app/app.animations.ts
+++ src/app/app.animations.ts
@@ -1,31 +1,27 @@
-import { animate, style, transition, trigger } from '@angular/animations';
+import { animate, group, keyframes, style, transition, trigger } from '@angular/animations';
 
 export const divTrigger = trigger('divTrigger', [
   // 'void => *  -->  ':enter'
    transition(':enter', [
-    style({
-      opacity: 0
-    }),
-    animate(500, style({
-      opacity: 1
-    }))
+     style({
+       width: '*',
+       height: '*'
+     }),
+     group([
+       animate(3000, style({
+         width: '200px',
+         height: '200px'
+       })),
+       animate(6000, keyframes([
+         style({ backgroundColor: 'blue' }),
+         style({ backgroundColor: 'yellow' }),
+         style({ backgroundColor: 'pink' })
+       ]))
+     ]),
+     animate(1000)
   ]),
   // '* => void' --> ':leave'
   transition(':leave', animate(500, style({
     opacity: 0
   })))
-]);
-// * здесь - это любое состояние;
-// void => * - переход из void в любое состояние;
-// * => void - переход из любого состояния в void
-
-export const changeWithTrigger = trigger('changeWith', [
-  transition('* => *', [
-    animate(1000, style({
-      width: '10px'
-    })),
-    animate(1000, style({
-      width: '*' // * обозначает изначальную ширину или высоту
-    }))
-  ])
 ]);
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { divTrigger, changeWithTrigger } from './app.animations';
+import { divTrigger } from './app.animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [divTrigger, changeWithTrigger]
+  animations: [divTrigger]
 })
 export class AppComponent {
   isVisible = false;
