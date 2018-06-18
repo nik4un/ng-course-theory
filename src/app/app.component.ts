3cfe2c069b8f0ad0a313547320dec8c51741b2ce
Nik <nik@iMac.local>
2018-06-18 10:23:50 +0300

    p1-b12-1 [Запуск тестов]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { divTrigger } from './app.animations';
-import { AnimationEvent } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  animations: [divTrigger]
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  isVisible = false;
-
-  onAnimationStart(event: AnimationEvent) {
-    console.log('Start: ', event);
-  }
-
-  onAnimationDane(event: AnimationEvent) {
-    console.log('Done: ', event);
-  }
+  title = 'app';
 }
