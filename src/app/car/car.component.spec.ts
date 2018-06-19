b690c20b96cfc80a9a40a854379a8af04e151a6c
nik4un <nik4un@yandex.ru>
2018-06-19 15:40:51 +0300

    p1-b12-3 [Тестирование сервиса. Инъекция]

--- src/app/car/car.component.spec.ts
+++ src/app/car/car.component.spec.ts
@@ -1,30 +1,51 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { CarComponent } from './car.component';
+import { CarService } from './car.service';
 
 describe('CarComponent', () => {
+  // хоршо бы вынести повторяющийся код типа:
+  // const fixture = TestBed.createComponent(CarComponent);
+  // для этого задаем переменную типа ComponentFixture, у которой в свою очередь тип CarComponent
+  let fixture: ComponentFixture<CarComponent>;
+  // и другую повторяющуюся переменную с соответствующим типом
+  let component: CarComponent;
 
   // 1. Регистрация комрпонента внутри тестов, как-бы эмулируем обычную регистрацию компонента
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [CarComponent]
     });
+    // и определяем их в beforeEach
+    fixture = TestBed.createComponent(CarComponent);
+    component = fixture.debugElement.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create the component «car»', () => {
-    // 2. Создание компонента внутри теста
-    const fixture = TestBed.createComponent(CarComponent);
-    // 3. Получаем экземпляр компонента
-    const car = fixture.debugElement.componentInstance;
-    // 4. Ожидаем получить
-    expect(car).toBeTruthy();
+      // 2. Создание компонента внутри теста
+    // const fixture = TestBed.createComponent(CarComponent);
+      // 3. Получаем экземпляр компонента
+    // const component = fixture.debugElement.componentInstance;
+      // 4. Ожидаем получить
+    expect(component).toBeTruthy();
   });
 
-  it('should render tag with title «header»', () => {
-    const fixture = TestBed.createComponent(CarComponent);
+  it('should render h1 tag with title «header»', () => {
+    // const fixture = TestBed.createComponent(CarComponent);
     // для того, чтобы angular применил изменеия, проишедшие в компонете в шаблоне
-    fixture.detectChanges();
-    const car = fixture.debugElement.nativeElement;
-    const h1 = car.querySelector('h1').textContent;
+    // fixture.detectChanges();
+    const nativeElement = fixture.debugElement.nativeElement;
+    const h1 = nativeElement.querySelector('h1').textContent;
     expect(h1).toEqual('My car header');
+  });
+
+  it('should inject CarService', () => {
+    // const fixture = TestBed.createComponent(CarComponent);
+    // const component = fixture.debugElement.componentInstance;
+      // инжектим сервис в тест, операция похожая на инжект сервиса в конструктор
+      // constructor(private carService: CarService) { }
+    const carService = fixture.debugElement.injector.get(CarService);
+    // fixture.detectChanges();
+    expect(component.isCarVisible).toEqual(carService.getVisibility());
   });
 });
